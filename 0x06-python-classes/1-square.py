134954c6b8e20e0ab0b2f346099a5ae36eac9c71
AkemMensah <141195emmanuel@gmail.com>
2023-06-27 13:45:47 -0700

    all tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define as class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
